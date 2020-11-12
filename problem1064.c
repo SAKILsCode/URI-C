7fe571d9e54ba040f308b8af9725a87368400254
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2020-11-12 17:46:57 +0600

    Probmem 1064 - Accepted

--- problem1064.c
+++ problem1064.c
@@ -4,10 +4,10 @@ int main() {
 
     float x[5];
     float sum, average;
-    scanf("%f\n%f\n%f\n%f\n%f\n%f", &x[0], &x[1], &x[2], &x[3], &x[4], &x[5]);
     int i, count = 0;
 
     for(i = 0; i <= 5; i++){
+        scanf("%f", &x[i]);
         
         if (x[i] > 0){
             count += 1;
