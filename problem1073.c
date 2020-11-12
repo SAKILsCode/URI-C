824675a45ca0a521d8c95e6b3e5326ab9335326e
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2020-11-12 21:38:52 +0600

    Problem 1073

---
+++ problem1073.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int i, n, x = 0;
+
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++){
+        if(i % 2 == 0){
+            printf("%d^2 = %d\n", i, i*i);
+        }
+    }
+
+    
+ 
+    return 0;
+}
