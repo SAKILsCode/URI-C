916ca67fac1620050ee01640ab2b4aaa06e7c301
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2020-11-15 00:03:28 +0600

    Problem 1133

---
+++ problem1133.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int x, y, i;
+    scanf("%d%d", &x, &y);
+
+    if(x < y){
+        for(i = x+1; i < y; i++){
+            if(i % 5 == 2){
+                printf("%d\n", i);
+            }else if(i % 5 == 3){
+                printf("%d\n", i);
+            }
+        }
+    }else if(x > y){
+        for(i = y+1; i < x; i++){
+            if(i % 5 == 2){
+                printf("%d\n", i);
+            }else if(i % 5 == 3){
+                printf("%d\n", i);
+            }
+        }
+    }
+
+    return 0;
+}
