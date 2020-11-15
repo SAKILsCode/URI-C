9e2a300b2e2c25b6d58bc11e6ef96b533f897efe
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2020-11-15 23:41:14 +0600

    Problem 1156

---
+++ problem1156.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    float s = 0, a = 1, dv;
+    int i = 1;
+
+    for(i = 1; i <= 39; i+=2){
+        dv = i / a;
+        s += dv;
+        a *= 2;
+    }
+    printf("%.2f\n", s); 
+    return 0;
+}
