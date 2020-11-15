9dab5f75c88e21ca330b3752a954f7bc4c84b20c
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2020-11-15 16:52:42 +0600

    Problem 1146

---
+++ problem1146.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x, i;
+
+    while (1)
+    {
+        scanf("%d", &x);
+
+        if (x == 0)
+        {
+            break;
+        }
+        else
+        {
+            for (i = 1; i <= x; i++)
+            if(i == x){
+                printf("%d", i);
+            }
+            else
+            {
+                printf("%d ", i);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
