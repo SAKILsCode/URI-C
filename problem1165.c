6e7c4cf9e83e86d328d275b1e4d0861879bf3dfc
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2020-11-16 23:20:34 +0600

    Problem 1165

---
+++ problem1165.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+
+    int n, i, x, j, count;
+
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        
+        scanf("%d", &x);
+        count = 0;
+        for(j = 1; j <= x; j++){
+            if(x % j == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+            printf("%d eh primo\n", x);
+        }else{
+            printf("%d nao eh primo\n", x);
+        }
+    }
+    return 0;
+}
