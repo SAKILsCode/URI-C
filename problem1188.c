3141ed3018ee9f513c6a5b09cb39b17bb07d4cfa
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2021-01-10 13:04:01 +0600

    Problem 1188

--- problem1188.c
+++ problem1188.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
- 
-int main() {
- 
-    double arr[12][12], sum = 0.0, av;
-    char operation[2];
-    int i, j, a, k, start = 5, end = 6;
 
-    scanf("%s", &operation);
-    for(i = 0; i <= 11; i++){
-        for(j = 0; j <= 11; j++){
+int main()
+{
+    int i, j, start = 5, end = 6;
+    double arr[12][12], sum = 0.0, counter = 0;
+    char indicator;
+
+    scanf("%c", &indicator);
+
+    for (i = 0; i < 12; i++)
+    {
+        for (j = 0; j < 12; j++)
+        {
             scanf("%lf", &arr[i][j]);
         }
     }
 
-    for(a = 7; a <= 11; a++){
-        for(k = start; k <= end; k++){
-            sum += arr[a][k];
+    for (i = 7; i <= 11; i++)
+    {
+        for (j = start; j <= end; j++)
+        {
+            sum += arr[i][j];
+            counter++;
         }
         start--;
         end++;
     }
 
-    if(operation[0] == 'S'){
+    if (indicator == 'S')
+    {
         printf("%.1lf\n", sum);
     }
-    else if(operation[0]=='M'){
-        av=sum/30.0;
-        printf("%.1lf\n", av);
+    if (indicator == 'M')
+    {
+        printf("%.1lf\n", sum / counter);
     }
- 
+
     return 0;
 }
-
